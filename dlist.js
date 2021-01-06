a8797a59a0e8127fdf86ebd06cec64a252998088
MR.jiang <2436732935@qq.com>
2021-01-06 15:50:16 +0800

    Update dlist.js

--- dlist.js
+++ dlist.js
@@ -17,7 +17,7 @@
 // You can optional change the response headers at the same time by using $done({body: modifiedBody, headers: modifiedHeaders}); only change the response headers is not allowed for script-response-body. The modifiedHeaders can be copied and modified from $response.headers, please do not change the content length, type and encoding field.
 // Response status can also be optional changed by using $done({body: modifiedBody, headers: modifiedHeaders, status: modifiedStatus}), the modifiedStatus should be like "HTTP/1.1 200 OK"
 
-var url = request.url;
+var url = $request.url;
 
 
 //var roleId = map[‘roleId’];
